4050d7f8ee71df41e40ed1659660336b92cb2b30
royas <tomroj@gmail.com>
2014-01-31 02:09:32 +0100

    Polish translation

---
+++ qa-history-lang-pl.php
@@ -0,0 +1,100 @@
+﻿<?php
+	return array(
+
+		'user_act_list_q_post'=>'wysłał pytanie',
+		'user_act_list_a_post'=>'wysłał odpowiedź',
+		'user_act_list_c_post'=>'wysłał komentarz',
+
+		'user_act_list_q_edit'=>'edytował pytanie',
+		'user_act_list_a_edit'=>'edytował odpowiedź',
+		'user_act_list_c_edit'=>'edytował komentarz',
+
+		'user_act_list_q_hide'=>'ukrył pytanie',
+		'user_act_list_a_hide'=>'ukrył odpowiedź',
+		'user_act_list_c_hide'=>'ukrył komentarz',
+
+		'user_act_list_q_reshow'=>'przywrócił pytanie',
+		'user_act_list_a_reshow'=>'przywrócił odpowiedź',
+		'user_act_list_c_reshow'=>'przywrócił komenarz',
+
+		'user_act_list_q_delete'=>'usunął pytanie',
+		'user_act_list_a_delete'=>'usunął odpowiedź',
+		'user_act_list_c_delete'=>'usunął komentarz',
+
+		'user_act_list_q_move'=>'przeniósł pytanie',
+		'user_act_list_a_to_c'=>'zmienił odpowiedź na komentarz',
+		'user_act_list_c_claim'=>'claimed a comment',
+
+		'user_act_list_q_claim'=>'claimed a question',
+		'user_act_list_a_claim'=>'claimed an answer',
+		'user_act_list_c_claim'=>'claimed a comment',
+
+		'user_act_list_q_flag'=>'zgłosił pytanie',
+		'user_act_list_a_flag'=>'zgłosił odpowiedź',
+		'user_act_list_c_flag'=>'zgłosił komentarz',
+
+		'user_act_list_q_unflag'=>'anulował zgłoszenie pytania',
+		'user_act_list_a_unflag'=>'anulował zgłoszenie odpowiedzi',
+		'user_act_list_c_unflag'=>'anulował zgłoszenie komentarza',
+
+		'user_act_list_in_q_flag'=>'pytanie zgłoszone',
+		'user_act_list_in_a_flag'=>'odpowiedź zgłoszona',
+		'user_act_list_in_c_flag'=>'komentarz zgłoszony',
+
+
+		'user_act_list_in_q_unflag'=>'anulowanie zgłoszenia pytania',
+		'user_act_list_in_a_unflag'=>'anulowanie zgłoszenia odpowiedzi',
+		'user_act_list_in_c_unflag'=>'anulowanie zgłoszenia komentarza',
+
+		'user_act_list_a_select'=>'wybrał najlepszą odpowiedź',
+		'user_act_list_a_unselect'=>'usunął wybór najlepszej odpowiedzi',
+
+		'user_act_list_in_a_select'=>'odpowiedź wybrana jako najlepsza',
+		'user_act_list_in_a_unselect'=>'usunięty wybór najlepszej odpowiedzi',
+
+		'user_act_list_in_q_vote_up'=>'otrzymał +1 za pytanie',
+		'user_act_list_in_q_vote_down'=>'otrzymał -1 za pytanie',
+		'user_act_list_in_a_vote_up'=>'otrzymał +1 za odpowiedź',
+		'user_act_list_in_a_vote_down'=>'otrzymał -1 za odpowiedź',
+		'user_act_list_q_vote_up'=>'zagłosował +1 na pytanie',
+		'user_act_list_q_vote_down'=>'zagłosował -1 na pytanie',
+		'user_act_list_a_vote_up'=>'zagłosował +1 na odpowiedź',
+		'user_act_list_a_vote_down'=>'zagłosował -1 na odpowiedź',
+
+		'user_act_list_q_vote_nil'=>'odwołał głos na pytanie',
+		'user_act_list_a_vote_nil'=>'odwołał głos na odpowiedź',
+
+		'user_act_list_in_q_vote_nil'=>'odwołany głos na pytanie',
+		'user_act_list_in_a_vote_nil'=>'odwołany głos na odpowiedź',
+
+		// special
+
+		'user_act_list_in_a_question'=>'odpowiedź na pytanie',
+		'user_act_list_in_c_question'=>'komentarz do pytania',
+		'user_act_list_in_c_answer'=>'komentarz do odpowiedzi',
+
+
+		'user_act_list_u_password'=>'ustawił hasło',
+		'user_act_list_u_reset'=>'zresetował hasło',
+		'user_act_list_u_save'=>'edytował profil',
+		'user_act_list_u_confirmed'=>'potwierdził adres email',
+		'user_act_list_u_edit'=>'edytował profil użytkownika:',
+		'user_act_list_u_level'=>'zmienił poziom użytkownika:',
+		'user_act_list_u_block'=>'zablokował użytkownika:',
+		'user_act_list_u_unblock'=>'odblokował użytkownika:',
+
+		'user_act_list_in_u_edit'=>'profil zmieniony',
+		'user_act_list_in_u_level'=>'poziom zmieniony na:',
+		'user_act_list_in_u_block'=>'zablokowany',
+		'user_act_list_in_u_unblock'=>'odblokowany',
+
+		'user_act_list_feedback'=>'wysłał uwagi',
+		'user_act_list_search'=>'szukał:',
+		'user_act_list_u_register'=>'zarejestrował się',
+
+		'user_act_list_badge_awarded'=>'^badge'
+	);
+	
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
